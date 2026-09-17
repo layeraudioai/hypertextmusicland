66c06ea9cfb50e47002f4361d0a394b44d6d1864
Brenjamin <xbcx@nintendo.com>
2026-09-16 21:22:50 -0400

    typo

--- aio.bat
+++ aio.bat
@@ -1,2 +1,2 @@
 @echo off
-npm install && npm run build:aio && del dist\assets\*.css && dist\assets\*.css && rmdir dist\assets
+npm install && npm run build:aio && del dist\assets\*.js && dist\assets\*.css && rmdir dist\assets
