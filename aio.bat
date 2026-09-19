51014f7f87295bfe5fa84b612866f0f5dd5ae970
Brenjamin <xbcx@nintendo.com>
2026-09-18 20:21:09 -0400

    stoopid

--- aio.bat
+++ aio.bat
@@ -1,6 +1,6 @@
 @echo off
 if /I "%~1"=="aio"  (
-    npm run build:aio && del dist\assets\*.js && del dist\assets\*.css && rmdir dist\assets
+    npm run build:aio && del dist\assets\*.js && del dist\assets\*.css && rmdir dist\assets && exit
 ) else (
-    npm run build
+    npm run build && exit
 )
