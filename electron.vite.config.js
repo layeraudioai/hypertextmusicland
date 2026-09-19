dc347fec27b8f9538de0fe9637dad2245136125b
Brenjamin <xbcx@nintendo.com>
2026-09-19 06:15:25 -0400

    exe fix

--- electron.vite.config.js
+++ electron.vite.config.js
@@ -21,7 +21,7 @@ export default defineConfig({
     renderer: {
         build: {
             rollupOptions: {
-                input: resolve(__dirname, 'index.html'),
+                input: resolve(__dirname, 'dist/index.html'),
             },
         },
     },
