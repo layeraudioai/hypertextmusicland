b7dde870c1be3033848e1589ad1ad7070af0fe00
Brenjamin <xbcx@nintendo.com>
2026-09-18 20:35:03 -0400

    aio

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   base: './', // Ensures relative paths in the generated index.html
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-  },
   plugins: [
     tailwindcss(),
     react()
